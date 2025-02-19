7ce1fbc597de7991f3cdf91597c27bbdc2491179
husseinrasti <husseinrasti@gmail.com>
2025-02-20 00:30:24 +0330

    change chain to testnet

--- src/app/const/chain.ts
+++ src/app/const/chain.ts
@@ -1,8 +1,8 @@
 import { defineChain } from "thirdweb";
 
 export const monadChain = defineChain({
-    id: 20143,
-    rpc: "https://rpc-devnet.monadinfra.com/rpc/3fe540e310bbb6ef0b9f16cd23073b0a",
+    id: 10143,
+    rpc: "https://testnet-rpc.monad.xyz/",
     nativeCurrency: {
         name: "Monad",
         symbol: "MON",
